00c4dec5f3f4eb00da34527b2e61cdc8b61a2aef
irinakulinich3712 <iryna.julinich@praguecollege.cz>
2021-05-13 01:20:59 +0300

    Registering models
     Giving access Django model records in the Django admin.
    Includes UserAdmin to render the convenient admin look for the User model.

--- english_courses/main/admin.py
+++ english_courses/main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Application, CustomUser, StudentGroup, Lesson, Announcement, Observation,\
+    Student
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+admin.site.register(Application)
+admin.site.register(CustomUser, UserAdmin)
+admin.site.register(Announcement)
+admin.site.register(StudentGroup)
+admin.site.register(Lesson)
+admin.site.register(Observation)
+admin.site.register(Student)
