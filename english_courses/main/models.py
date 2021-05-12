0fe3d1a3e84f771e06329f928e4f21bafa3fd2ac
irinakulinich3712 <iryna.julinich@praguecollege.cz>
2021-05-12 22:56:53 +0300

    Adding Application model
    The model with fields required for the Application table - information collected from website visitors who want enroll. Each Application object is represented with the date it was submitted and full name of the child to be enrolled.

--- english_courses/main/models.py
+++ english_courses/main/models.py
@@ -10,3 +10,18 @@ class StudentGroup(models.Model):
 
     def __str__(self):
         return '%s%s' % (self.year, self.name)
+
+
+class Application(models.Model):
+    created_at = models.DateField(auto_now_add=True)
+    f_name = models.CharField(max_length=20, verbose_name="Student's first name")
+    l_name = models.CharField(max_length=30, verbose_name="Student's last name")
+    age = models.IntegerField(verbose_name="Student's age")
+    parent_tel_numb = models.CharField(max_length=13, verbose_name="Parent's telephone number")
+    parent_email = models.EmailField(max_length=254, verbose_name="Parent's email")
+    parent_f_name = models.CharField(max_length=20, verbose_name="Parent's first name")
+    parent_patronimic = models.CharField(max_length=30, verbose_name="Parent's patronimic")
+    parent_l_name = models.CharField(max_length=30, verbose_name="Parent's last name")
+
+    def __str__(self):
+        return '%s : %s %s' % (self.created_at, self.f_name, self.l_name)
