e8b8741e85fbe5ab83d993ac0d0948f762b46674
irinakulinich3712 <iryna.julinich@praguecollege.cz>
2021-05-13 01:10:00 +0300

    Creating Student model
    A model for representing information specific for Student users, which is not required for other types of users(admin, teacher).
    Is identified by the CustomUser object.
    The way to create a string representing the Student object will be defined later

--- english_courses/main/models.py
+++ english_courses/main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 
 class StudentGroup(models.Model):
@@ -20,6 +21,17 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return '%s %s' % (self.last_name, self.first_name)
+
+
+class Student(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
+    parent_tel_numb = models.CharField(max_length=13, verbose_name="Parent's telephone number")
+    parent_f_name = models.CharField(max_length=20, verbose_name="Parent's first name")
+    parent_patronimic = models.CharField(max_length=30, verbose_name="Parent's patronimic")
+    parent_l_name = models.CharField(max_length=30, verbose_name="Parent's last name")
+    student_group = models.ForeignKey(StudentGroup, to_field='id', on_delete=models.SET_NULL, blank=True, null=True)
+
+    # def __str__(self):
 
 
 class Application(models.Model):
