671b2af859736fbad6c1ec76a5e7a6d0010cd1f7
irinakulinich3712 <iryna.julinich@praguecollege.cz>
2021-05-18 17:52:43 +0300

    Update lessons.py
    Function to render template with lessons of particular group

---
+++ english_courses/main/views/lessons.py
@@ -0,0 +1,18 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import render
+from datetime import datetime, timedelta
+from ..models import StudentGroup, Lesson
+from django.http import HttpResponse
+
+
+def lessons(request, g_id):
+    try:
+        group_obj = StudentGroup.objects.get(id=g_id)
+
+        group_lessons_list = Lesson.objects.filter(student_group=g_id,
+                                              created_at__gte=datetime.now() - timedelta(days=60)) \
+            .order_by('created_at')
+
+        return render(request, 'main/lessons.html', {'group_lessons': group_lessons_list, 'group': group_obj})
+    except ObjectDoesNotExist:
+        return HttpResponse("This groups does not exist")
