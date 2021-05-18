4a8f41c3b94a2c51a6c556e9f78f3be7bc09d31c
irinakulinich3712 <iryna.julinich@praguecollege.cz>
2021-05-18 17:49:49 +0300

    Update home.py
    Retrieve only announcements created within the last 60 days

--- english_courses/main/views/home.py
+++ english_courses/main/views/home.py
@@ -1,8 +1,11 @@
+import datetime
+
 from django.shortcuts import render, redirect
 from ..models import Announcement
 
 
 def index(request):
-    announcements = Announcement.objects.order_by('created_at')
+    announcements = Announcement.objects.filter(created_at__gte=datetime.now() - datetime.timedelta(days=60)).order_by(
+        'created_at')
 
     return render(request, 'main/index.html', {'announcements': announcements})
