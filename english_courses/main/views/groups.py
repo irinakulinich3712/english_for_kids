76b8721bf102e5694483e182dd9565ad78d641c6
irinakulinich3712 <iryna.julinich@praguecollege.cz>
2021-05-18 17:51:29 +0300

    Update groups.py
    Adding function group to render template for individual groups

--- english_courses/main/views/groups.py
+++ english_courses/main/views/groups.py
@@ -1,9 +1,25 @@
 from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from ..models import StudentGroup
+from ..models import StudentGroup, Student
 
 
 def all_groups(request):
-    group_names = StudentGroup.objects.order_by('year')
+    groups_list = StudentGroup.objects.order_by('year')
 
-    return render(request, 'main/all_groups.html', {'group_names': group_names})
+    return render(request, 'main/all_groups.html', {'groups': groups_list})
+
+
+def group(request, g_id):
+    try:
+        group_obj = StudentGroup.objects.get(id=g_id)
+
+        students_list = Student.objects.filter(student_group_id=g_id). \
+            select_related('user').values('user_id',
+                                          'user__last_name',
+                                          'user__first_name').order_by('user__last_name')
+
+        return render(request, 'main/group.html', {'group': group_obj, 'students': students_list})
+    except ObjectDoesNotExist:
+        return HttpResponse("This group could not be found")
