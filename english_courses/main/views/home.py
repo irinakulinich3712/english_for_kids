287449ddb1ee6a1d41d62a9f4596ddcd8e48f659
irinakulinich3712 <iryna.julinich@praguecollege.cz>
2021-05-18 18:13:42 +0300

    Renaming the variable for announcements

--- english_courses/main/views/home.py
+++ english_courses/main/views/home.py
@@ -5,7 +5,7 @@ from ..models import Announcement
 
 
 def index(request):
-    announcements = Announcement.objects.filter(created_at__gte=datetime.now() - datetime.timedelta(days=60)).order_by(
+    announcements_list = Announcement.objects.filter(created_at__gte=datetime.now() - datetime.timedelta(days=60)).order_by(
         'created_at')
 
-    return render(request, 'main/index.html', {'announcements': announcements})
+    return render(request, 'main/index.html', {'announcements': announcements_list})
