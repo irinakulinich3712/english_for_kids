f4f0bee97f244b098f738ce2b58549b0b8d6380f
irinakulinich3712 <iryna.julinich@praguecollege.cz>
2021-05-19 14:12:51 +0300

    Function for individual application
    Function one_application was created for rendering template where a selected application is fully displayed

--- english_courses/main/views/applications.py
+++ english_courses/main/views/applications.py
@@ -5,3 +5,8 @@ from ..models import Application
 def applications(request):
     applications_list = Application.objects.order_by('created_at')
     return render(request, 'main/applications.html', {'applications': applications_list})
+
+
+def one_application(request, ap_id):
+    application_obj = Application.objects.get(id=ap_id)
+    return render(request, 'main/application.html', {'applications_list': application_obj})
