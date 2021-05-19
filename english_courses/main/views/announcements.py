26fe99ce00e51dfff0e14a651a3302751506af03
irinakulinich3712 <iryna.julinich@praguecollege.cz>
2021-05-19 14:09:17 +0300

    Fixed imports
    Fixing datetime imports

--- english_courses/main/views/announcements.py
+++ english_courses/main/views/announcements.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timedelta
 
 from django.shortcuts import render
 from ..models import Announcement
@@ -6,7 +6,7 @@ from ..models import Announcement
 
 def announcements(request):
     announcements_list = Announcement.objects.filter(
-        created_at__gte=datetime.now() - datetime.timedelta(days=60)).order_by(
+        created_at__gte=datetime.now() - timedelta(days=60)).order_by(
         'created_at')
 
     return render(request, 'main/announcements.html', {'announcements': announcements_list})
--- english_courses/main/views/home.py
+++ english_courses/main/views/home.py
@@ -1,11 +1,11 @@
-import datetime
+from datetime import datetime, timedelta
 
 from django.shortcuts import render, redirect
 from ..models import Announcement
 
 
 def index(request):
-    announcements_list = Announcement.objects.filter(created_at__gte=datetime.now() - datetime.timedelta(days=60)).order_by(
+    announcements_list = Announcement.objects.filter(created_at__gte=datetime.now() - timedelta(days=60)).order_by(
         'created_at')
 
     return render(request, 'main/index.html', {'announcements': announcements_list})
