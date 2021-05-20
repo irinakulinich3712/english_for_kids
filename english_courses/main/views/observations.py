bb780bb7a05ddd413c3eb08958bc287fedbcdf6a
irinakulinich3712 <iryna.julinich@praguecollege.cz>
2021-05-20 20:29:56 +0300

    Update observations.py
    Function to delete an observation

--- english_courses/main/views/observations.py
+++ english_courses/main/views/observations.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from ..models import Observation
 
 
@@ -6,3 +7,10 @@ def observations(request, s_id):
     observations_list = Observation.objects.filter(student=s_id).order_by('created_at')
 
     return render(request, 'main/observations.html', {'observations': observations_list})
+
+
+def delete_observation(request, s_id, o_id):
+    observation = Observation.objects.get(id=o_id)
+    observation.delete()
+    messages.success(request, "An observation has been deleted successfully")
+    return redirect('observations', s_id=s_id)
