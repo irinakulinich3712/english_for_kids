03a9f754a9269bfc3e70a9c9c75bc2ac636f51f5
irinakulinich3712 <iryna.julinich@praguecollege.cz>
2021-05-21 16:07:31 +0300

    Create form for creating a student
    An inbuilt UserCreationForm for the CustomUser model. Includes the same fields as the Application form + username and student group fileds. Each field has it's limitations(min/max length) defined, as well as form widgets. The password fields are excluded, as they are to be automatically generated in the corresponding view function.  The save() function is overridden as besides saving the CustomUser data, it is necessary to save Student's data. For the created CustomUser in this function, a corresponding related Student object is created. In addition, in this function a user group is set to 'student' value and the password is to be set from the function's parameter.

--- english_courses/main/forms.py
+++ english_courses/main/forms.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
+from django.db import transaction
+from django.contrib.auth.models import Group
 
 from .models import Application, Student, StudentGroup, Lesson, Announcement, Observation, CustomUser
-from django.forms import ModelForm, TextInput, NumberInput, Textarea, EmailInput
+from django.forms import ModelForm, TextInput, Textarea, EmailInput, Select, ModelChoiceField, \
+    NumberInput, CharField, EmailField
 
 
 class CreateGroupForm(ModelForm):
@@ -221,3 +225,116 @@ class CreateApplicationForm(ModelForm):
                 'label': "Parent's last name"
             }),
         }
+
+
+class CreateStudentForm(UserCreationForm):
+    username = CharField(max_length=20, label="Username", widget=TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': "Enter a username"
+    }))
+    first_name = CharField(max_length=20, label="First name", widget=TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': "Enter the student's first name"
+    }))
+    last_name = CharField(max_length=30, label="Last name", widget=TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': "Enter the student's last name"
+    }))
+    email = EmailField(max_length=200, label="E-mail", widget=EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': "Enter the parent's e-mail address"
+    }))
+    parent_f_name = CharField(max_length=20, label="Parent's first name", widget=TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': "Enter the parent's first name"
+    }))
+    parent_patronimic = CharField(max_length=30, label="Parent's patronimic", required=False,
+                                  help_text="Parent's patronimic can be omitted", widget=TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': "Enter the parent's patronimic"
+    }))
+    parent_l_name = CharField(max_length=30, label="Parent's last name", widget=TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': "Enter the parent's last name"
+    }))
+    parent_tel_numb = CharField(max_length=10, min_length=10, label="Parent's telephone number", widget=TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': "Enter the parent's telephone number"
+    }))
+
+    student_group = ModelChoiceField(queryset=StudentGroup.objects.all(), label="Student group",
+                                     required=False, help_text="Student's group can be omitted",
+                                     widget=Select(attrs={
+                                         'class': 'form-control',
+                                         'placeholder': "Choose the student's groups"
+                                     }))
+
+    def clean_f_name(self):
+        f_name = self.cleaned_data["f_name"]
+
+        if not f_name.isalpha():
+            raise ValidationError("The first name should only contain letters")
+        return f_name
+
+    def clean_l_name(self):
+        l_name = self.cleaned_data["l_name"]
+
+        if not l_name.isalpha():
+            raise ValidationError("The last name should only contain letters")
+        return l_name
+
+    def clean_parent_tel_numb(self):
+        parent_tel_numb = self.cleaned_data["parent_tel_numb"]
+
+        if not parent_tel_numb.isdecimal():
+            raise ValidationError("Telephone number should only contain digits")
+        return parent_tel_numb
+
+    def clean_parent_f_name(self):
+        parent_f_name = self.cleaned_data["parent_f_name"]
+
+        if not parent_f_name.isalpha():
+            raise ValidationError("The first name should only contain letters")
+        return parent_f_name
+
+    def clean_parent_patronimic(self):
+        parent_patronimic = self.cleaned_data["parent_patronimic"]
+
+        if not parent_patronimic.isalpha():
+            raise ValidationError("The patronimic should only contain letters")
+        return parent_patronimic
+
+    def clean_parent_l_name(self):
+        parent_l_name = self.cleaned_data["parent_l_name"]
+
+        if not parent_l_name.isalpha():
+            raise ValidationError("The last name should only contain letters")
+        return parent_l_name
+
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ('username', 'first_name', 'last_name',
+                  'email', 'parent_f_name', 'parent_patronimic', 'parent_l_name',
+                  'parent_tel_numb', 'student_group')
+
+    def __init__(self, *args, **kwargs):
+        super(CreateStudentForm, self).__init__(*args, **kwargs)
+        self.fields.pop('password1')
+        self.fields.pop('password2')
+
+    @transaction.atomic
+    def save(self, pword):
+        self.cleaned_data['password1'] = pword
+        user = super().save()
+        group = Group.objects.get(name='student')
+        user.groups.add(group)
+        user.save()
+
+        student = Student.objects.create(user=user)
+        student.parent_f_name = self.cleaned_data.get('parent_f_name')
+        student.parent_patronimic = self.cleaned_data.get('parent_patronimic')
+        student.parent_l_name = self.cleaned_data.get('parent_l_name')
+        student.parent_tel_numb = self.cleaned_data.get('parent_tel_numb')
+        student.student_group = self.cleaned_data.get('student_group')
+        student.save()
+        return user
