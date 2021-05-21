73bf2ab8ee02c23556ebe0edae68028aca777c99
irinakulinich3712 <iryna.julinich@praguecollege.cz>
2021-05-21 15:24:01 +0300

    Create forms for creating and editing student group's lessons
    Two identical Django modelForms with the only field for the lesson's content,  with form widget set for the field (Textarea). The Lesson model fields created_at and student_group are excluded from the model form, as date of creation is set to be generated automatically and student_group should be defined automatically, by setting it in the view of that group's lessons

--- english_courses/main/forms.py
+++ english_courses/main/forms.py
@@ -1,5 +1,5 @@
 from .models import Application, Student, StudentGroup, Lesson, Announcement, Observation, CustomUser
-from django.forms import ModelForm, TextInput, NumberInput
+from django.forms import ModelForm, TextInput, NumberInput, Textarea
 
 
 class CreateGroupForm(ModelForm):
@@ -31,5 +31,32 @@ class EditGroupForm(ModelForm):
             "name": TextInput(attrs={
                 'class': 'modal-form__input',
                 'placeholder': "Enter the group name"
+            }),
+        }
+
+
+class CreateLessonForm(ModelForm):
+    class Meta:
+        model = Lesson
+        exclude = ("created_at", "student_group")
+        fields = ["task"]
+        widgets = {
+            "task": Textarea(attrs={
+                'class': 'modal-form__input',
+                'placeholder': "Enter the task"
+            }),
+        }
+
+
+class EditLessonForm(ModelForm):
+    class Meta:
+        model = Lesson
+        exclude = ("created_at", "group")
+        fields = ["task"]
+        widgets = {
+            "task": Textarea(attrs={
+                'id': 'edit-lesson-task',
+                'class': 'modal-form__input',
+                'placeholder': "Enter the task"
             }),
         }
