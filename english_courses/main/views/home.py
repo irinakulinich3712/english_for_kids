1521311f08d0b143b776cac7b078225ca01d71f5
irinakulinich3712 <iryna.julinich@praguecollege.cz>
2021-05-21 17:32:38 +0300

    Add application creation form
    The form to the template and processed in the view function

--- english_courses/main/views/home.py
+++ english_courses/main/views/home.py
@@ -1,6 +1,9 @@
 from datetime import datetime, timedelta
 
+from django.contrib import messages
 from django.shortcuts import render, redirect
+
+from ..forms import CreateApplicationForm
 from ..models import Announcement
 
 
@@ -8,4 +11,18 @@ def index(request):
     announcements_list = Announcement.objects.filter(created_at__gte=datetime.now() - timedelta(days=60)).order_by(
         'created_at')
 
-    return render(request, 'main/index.html', {'announcements': announcements_list})
+    if request.method == 'POST':
+        create_form = CreateApplicationForm(request.POST)
+        if create_form.is_valid():
+            create_form.save()
+            messages.success(request, "Your application has been send successfully")
+        else:
+            messages.error(request, "The form has been filled incorrectly")
+    else:
+        create_form = CreateApplicationForm()
+    context = {
+        'create_form': create_form
+    }
+
+    return render(request, 'main/index.html', {'announcements': announcements_list,
+                                               'create_form': context['create_form']})
