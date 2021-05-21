7b4e71a0ff844d1391247ac7688f7bd9cbf6223b
irinakulinich3712 <iryna.julinich@praguecollege.cz>
2021-05-21 17:40:00 +0300

    Add lessons editing and creation form to the lessons view
    Both forms are rendered to the template, but only the creation form is processed in the view function

--- english_courses/main/views/lessons.py
+++ english_courses/main/views/lessons.py
@@ -2,6 +2,8 @@ from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, redirect
 from datetime import datetime, timedelta
+
+from ..forms import CreateLessonForm
 from ..models import StudentGroup, Lesson
 from django.http import HttpResponse
 
@@ -14,7 +16,28 @@ def lessons(request, g_id):
                                                    created_at__gte=datetime.now() - timedelta(days=60)) \
             .order_by('created_at')
 
-        return render(request, 'main/lessons.html', {'group_lessons': group_lessons_list, 'group': group_obj})
+        edit_form = EditLessonForm()
+
+        if request.method == 'POST':
+            create_form = CreateLessonForm(request.POST)
+            if create_form.is_valid():
+                create_form.instance.student_group = StudentGroup.objects.get(id=g_id)
+                create_form.save()
+                messages.success(request, "The lesson has been created successfully")
+            else:
+                messages.error(request, "The form has been filled incorrectly")
+        else:
+
+            create_form = CreateLessonForm()
+
+        context = {
+            'create_form': create_form,
+            'edit_form': edit_form
+        }
+
+        return render(request, 'main/lessons.html', {'group_lessons': group_lessons_list, 'group': group_obj,
+                                                     'create_form': context['create_form'],
+                                                     'edit_form': context['edit_form']})
     except ObjectDoesNotExist:
         return HttpResponse("This groups does not exist")
 
