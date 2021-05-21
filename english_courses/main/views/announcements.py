7f315e1aef33b1408868ab2bcefbc912335c9455
irinakulinich3712 <iryna.julinich@praguecollege.cz>
2021-05-21 17:22:38 +0300

    Add editing and creation forms to the announcements view
    Both forms are rendered to the template, but only the creation form is processed in the view function

--- english_courses/main/views/announcements.py
+++ english_courses/main/views/announcements.py
@@ -2,15 +2,36 @@ from datetime import datetime, timedelta
 
 from django.contrib import messages
 from django.shortcuts import render, redirect
+
+from ..forms import CreateAnnouncementForm, EditAnnouncementForm
 from ..models import Announcement
 
 
 def announcements(request):
-    announcements_list = Announcement.objects.filter(
-        created_at__gte=datetime.now() - timedelta(days=60)).order_by(
+    announcements_list = Announcement.objects.filter(created_at__gte=datetime.now() - timedelta(days=60)).order_by(
         'created_at')
+    edit_form = EditAnnouncementForm()
 
-    return render(request, 'main/announcements.html', {'announcements': announcements_list})
+    if request.method == 'POST':
+        create_form = CreateAnnouncementForm(request.POST)
+        if create_form.is_valid():
+            create_form.save()
+            messages.success(request, 'You have added an announcement successfully')
+            return redirect('announcements')
+        else:
+            messages.error(request, 'You have not filled the form correctly')
+
+    else:
+        create_form = CreateAnnouncementForm()
+
+    context = {
+        'create_form': create_form,
+        'edit_form': edit_form
+    }
+
+    return render(request, 'main/announcements.html', {'announcements': announcements_list,
+                                                       'create_form': context['create_form'],
+                                                       'edit_form': context['edit_form']})
 
 
 def delete_announcement(request, an_id):
