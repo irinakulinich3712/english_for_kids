78243f21cfee15b0794fc4af86f934fb10d0d48c
irinakulinich3712 <iryna.julinich@praguecollege.cz>
2021-05-22 14:05:38 +0300

    Create function for authenticating a user
    If a request method is POST, the data from the login form is collected, validated and checked by the Django authentication system. In case of successful login, if the user is a teacher, they are redirected to all groups page, if a student - to the student's teacher's observations page

---
+++ english_courses/main/views/authentication.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+
+from ..forms import LoginForm
+from ..models import Student
+
+
+def login_user(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    messages.success(request, 'You have been logged in successfully')
+                    if user.groups.filter(name='teacher').exists():
+                        return redirect('groups')
+                    if user.groups.filter(name='student').exists():
+                        return redirect('observations', s_id=user.id)
+                else:
+                    messages.error(request, 'Disabled account')
+        else:
+            messages.error(request, 'You have not filled the form correctly')
+    else:
+        form = LoginForm()
+    context = {
+        'form': form
+    }
+
+    return render(request, 'main/login.html', {'form': context['form']})
+
