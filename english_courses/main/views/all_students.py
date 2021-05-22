27ebfab0f48e82b2cd4467e0dd336716e8efb2a4
irinakulinich3712 <iryna.julinich@praguecollege.cz>
2021-05-22 13:32:39 +0300

    Create view function for creating a new student
    The function renders the template for the student creation form and processes it then. If there is a POST request - the data from the CreateStudentForm is processed and if it's valid - a user password is generated and passed to the the save method of that form, which then creates the user. In addition, user's username and email is extracted from the form and passed used to send the username and password to the newly created user. In case of successes or errors the teacher get's the corresponding messages.

--- english_courses/main/views/all_students.py
+++ english_courses/main/views/all_students.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from smtplib import SMTPException
+
+from django.conf import settings
+import random
+import string
+from django.contrib import messages
+from django.contrib.auth.hashers import make_password
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+
+from ..forms import CreateStudentForm
 from ..models import Student, StudentGroup
 
 
@@ -18,3 +28,35 @@ def all_students(request):
                                            StudentGroup.objects.get(id=s['student_group_id']).name)
 
     return render(request, 'main/all_students.html', {'students': students_list})
+
+
+def new_student(request):
+    password_characters = string.ascii_letters + string.digits + string.punctuation
+    if request.method == 'POST':
+        create_form = CreateStudentForm(request.POST)
+        if create_form.is_valid():
+            password_string = ''.join(random.choice(password_characters) for i in range(10))
+            password = make_password(password_string, hasher='default')
+            create_form.save(password)
+            user = create_form.cleaned_data.get('username')
+            user_email = create_form.cleaned_data.get('email')
+            try:
+                send_mail("English for kids student account credentials",
+                          'Username: %s; Password: %s' % (user, password_string),
+                          settings.EMAIL_HOST_USER,
+                          [user_email], fail_silently=False)
+            except SMTPException:
+                messages.error(request, 'There was a problem with sending e-mail')
+
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('all_students')
+        else:
+            messages.error(request, 'The form has been filled incorrectly')
+
+    else:
+        create_form = CreateStudentForm()
+    context = {
+        'create_form': create_form
+    }
+
+    return render(request, 'main/new_student.html', {'create_form': context['create_form']})
