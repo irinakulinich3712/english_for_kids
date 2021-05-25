2b50e7766af6b744b61a690d8dcad4b94b9c5d91
irinakulinich3712 <iryna.julinich@praguecollege.cz>
2021-05-25 09:34:12 +0300

    Change admin site url

--- english_courses/urls.py
+++ english_courses/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('english-courses-management/', admin.site.urls),
     path('', include('main.urls'))
 ]
