cee61946f8da2a7fb5f3983fe5895400b87bbc7a
irinakulinich3712 <iryna.julinich@praguecollege.cz>
2021-05-25 09:24:55 +0300

    Defining allowed hosts in environmental variables

--- english_courses/english_courses/settings.py
+++ english_courses/english_courses/settings.py
@@ -7,7 +7,9 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ['']
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 ADMINS = eval(config('ADMINS'))
 
