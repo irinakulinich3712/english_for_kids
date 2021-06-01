5204b7244a35958e5586b16f1f39acc42d8e54cb
Iryna <44743819+irinakulinich3712@users.noreply.github.com>
2021-06-01 23:35:37 +0300

    Update burgerAcc.js

--- main/static/main/js/burgerAcc.js
+++ main/static/main/js/burgerAcc.js
@@ -6,6 +6,7 @@
         if (document.querySelector('.sidebar__icon-menu-btn')) {
             document.querySelector('.sidebar__icon-menu-btn').addEventListener('click', () => {
             document.querySelector('.sidebar').classList.add('sidebar--active');
+            document.querySelector('.sidebar__icon-menu-btn').classList.add('sidebar__icon-menu-btn--hide');
         })
         }
 
@@ -13,6 +14,7 @@
         if (document.querySelector('.sidebar__icon-cancel-btn')) {
             document.querySelector('.sidebar__icon-cancel-btn').addEventListener('click', () => {
             document.querySelector('.sidebar').classList.remove('sidebar--active');
+            document.querySelector('.sidebar__icon-menu-btn').classList.remove('sidebar__icon-menu-btn--hide');
         });
         }
 
